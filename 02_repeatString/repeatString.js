ac62ba4b9f57c51194c256e9e59381416f8313ff
Adrian Gaile Singh <adriangailee@gmail.com>
2022-05-30 20:20:58 +0800

    initial commit

---
+++ 02_repeatString/repeatString.js
@@ -0,0 +1,15 @@
+const repeatString = function(word, times) {
+
+    if (times < 0) return 'ERROR';
+
+    let string = '';
+    
+    for (let i=0; i < times; i++ ) {
+        string += word;
+    }
+
+return string;
+};
+
+// Do not edit below this line
+module.exports = repeatString;
